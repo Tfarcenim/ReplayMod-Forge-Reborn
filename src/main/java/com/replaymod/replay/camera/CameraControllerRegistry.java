34bafce7449699d3dfce9f0d29693d9e9cb8b6a1
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-12-13 14:27:20 -0500

    Update CameraControllerRegistry.java

--- src/main/java/com/replaymod/replay/camera/CameraControllerRegistry.java
+++ src/main/java/com/replaymod/replay/camera/CameraControllerRegistry.java
@@ -11,13 +11,13 @@ import com.google.common.base.Preconditions;
 import com.replaymod.replay.Setting;
 
 public class CameraControllerRegistry {
-	private final Map<String, Function<CameraEntity, CameraController>> constructors = new ConcurrentHashMap();
+	private final Map<String, Function<CameraEntity, CameraController>> constructors = new ConcurrentHashMap<>();
 
 	public void register(String name, Function<CameraEntity, CameraController> constructor) {
 		Preconditions.checkState(!this.constructors.containsKey(name),
 				"Controller " + name + " is already registered.");
 		this.constructors.put(name, constructor);
-		Setting.CAMERA.setChoices(new ArrayList(this.getControllers()));
+		Setting.CAMERA.setChoices(new ArrayList<>(this.getControllers()));
 	}
 
 	public Set<String> getControllers() {
@@ -25,6 +25,6 @@ public class CameraControllerRegistry {
 	}
 
 	public CameraController create(String name, CameraEntity cameraEntity) {
-		return (CameraController) ((Function) this.constructors.get(name)).apply(cameraEntity);
+		return ( this.constructors.get(name)).apply(cameraEntity);
 	}
 }
